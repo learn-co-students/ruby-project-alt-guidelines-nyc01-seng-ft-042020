dc5b44b925fecaaabdde5c5849faf0918a82543d
Jessica <github email address>
2020-05-05 13:44:54 -0400

    created_models

---
+++ app/models/assignment.rb
@@ -0,0 +1,4 @@
+class Assignment < ActiveRecord::Base
+    belongs_to :student
+    belongs_to :teacher
+end
---
+++ db/migrate/20200505174003_create_assignments.rb
@@ -0,0 +1,4 @@
+class CreateAssignments < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
