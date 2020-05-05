6991cbb84645d92dad4f2a221844e1c214eade94
Jessica <github email address>
2020-05-05 16:30:18 -0400

    greeting

--- bin/run.rb
+++ bin/run.rb
@@ -4,3 +4,15 @@ require_relative '../config/environment'
 
 cli = CommandLineInterface.new
 cli.greeting
+
+puts "Are you a student or a teacher?"
+puts "Type in student or teacher to begin:"
+type_of_user = gets.chomp.downcase
+if type_of_user = "student"
+    
+elsif type_of_user = "teacher"
+
+elsif 
+    puts "Invalid input, please try again"
+end
+binding.pry
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,4 +2,11 @@ class CommandLineInterface
     def greeting
         puts "Welcome to the Assignment Portal!"
     end
+
+    def run
+        greeting
+        
+    end
+
 end
+
