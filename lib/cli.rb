68bcdd55007e3a874fe2761a7da361cbe5aa749f
Jessica <github email address>
2020-05-05 21:05:53 -0400

    assignments method skeleton

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,28 +3,6 @@ class CommandLineInterface
         puts "Welcome to the Assignment Portal!"
     end
 
-    def student_user #student user welcome message
-        puts "Type in your full name to begin:"
-        student_user = gets.chomp.titleize
-        if Student.find_by(name: student_user)
-            puts "Welcome #{student_user.titleize}!"
-            self.student(student_user)
-        else 
-            puts "You are not authorized to access the portal. Please contact student services for additional support"
-        end
-    end
-
-    def teacher_user #teacher user welcome message
-        puts "Type in your full name to begin:"
-        teacher_user = gets.chomp.titleize
-        if Teacher.find_by(name: teacher_user)
-            puts "Welcome #{teacher_user.titleize}!"
-            self.teacher(teacher_user)
-        else 
-            puts "You are not authorized to access the portal. Please contact your administrator for additional support"
-        end
-    end
-
     def run
         greeting
         puts "Are you a student or a teacher?"
@@ -42,6 +20,34 @@ class CommandLineInterface
             puts "Have a great day!"
         end
     end
+
+    def student_user #student user welcome message
+        puts "Type in your full name to begin:"
+        student_user = gets.chomp.titleize
+        if Student.find_by(name: student_user)
+            puts "Welcome #{student_user.titleize}!"
+            
+        else 
+            puts "You are not authorized to access the portal. Please contact student services for additional support"
+        end
+    end
+
+    # def student_assignments(student_user) #student user viewing assignments
+    # end
+
+    def teacher_user #teacher user welcome message
+        puts "Type in your full name to begin:"
+        teacher_user = gets.chomp.titleize
+        if Teacher.find_by(name: teacher_user)
+            puts "Welcome #{teacher_user.titleize}!"
+            
+        else 
+            puts "You are not authorized to access the portal. Please contact your administrator for additional support"
+        end
+    end
+
+    # def teacher_assignments(teacher_user) #teacher user viewing assignments
+    # end
 
     # def run
     #     greeting
