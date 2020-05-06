b467242483ae73486b3b9eafbf01a00dc83f66ec
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-05-06 14:58:00 -0400

    second commit migrated sschema

---
+++ db/migrate/03_create_recipes_ingredients.rb
@@ -0,0 +1,8 @@
+class CreateRecipesIngredients < ActiveRecord::Migration[4.2]
+    def change
+      create_table :recipes_ingredients do |t|
+        t.integer :recipe_id
+        t.integer :ingredient_id
+      end
+    end
+  end
