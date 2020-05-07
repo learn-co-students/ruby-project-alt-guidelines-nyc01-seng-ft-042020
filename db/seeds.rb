529d9bf75cd8b1bc0a97f8a1357e2dfdbc83d3c8
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-05-06 22:18:19 -0400

    added cli

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,62 @@
+require_relative "../app/models/ingredient.rb"
+require_relative "../app/models/recipe.rb"
+require_relative "../app/models/recipes_ingredients.rb"
 
 Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+Ingredient.create(name: Faker::Name.name)
+
+
 Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+Recipe.create(name: Faker::Company.name)
+
 RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+RecipesIngredients.create(recipe_id: Recipe.all.sample.id, ingredient_id: Ingredient.all.sample.id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
